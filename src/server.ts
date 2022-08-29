3d1cd578f89334a5425c4224b2b581e5ad212518
Vinícius Vicari <vicari_snoway@hotmail.com>
2022-08-29 01:26:40 -0300

    add dotenv and urlencoded on server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
+import * as dotenv from 'dotenv';
 import cors from "cors";
 import "reflect-metadata";
 import express from "express";
 import AppDataSource from "./database/dataSource";
 import { routes } from "./routes";
 
+dotenv.config();
 const app = express();
 const PORT = process.env.TYPEORM_PORT;
 
-app.use(express.json(), cors({
-  origin: '*'
-}),
-routes);
+app.use(
+  express.json(),
+  cors({
+    origin: "*",
+  }),
+  express.urlencoded({ extended: true }),
+  routes
+);
 
 AppDataSource.initialize().then(() => {
   app.listen(PORT, () => console.log(`Server is running on port: ${PORT}.`));
