4a2c063f84901bb33f7a733568d28d0e6e4b5799
Vinícius Vicari <vicari_snoway@hotmail.com>
2022-08-29 01:28:10 -0300

    add upload middleware on routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,51 +1,73 @@
 import { Router } from "express";
 import { AddressController } from "./controllers/AddressController";
 import { ClientController } from "./controllers/ClientController";
+import { LoginController } from "./controllers/LoginController";
 import { ProductController } from "./controllers/ProductController";
 import { ProviderController } from "./controllers/ProviderController";
 import { SchedulingController } from "./controllers/SchedulingController";
+import { Upload } from "./middlewares/upload";
+import multer from "multer";
 
+const multerConfig = require("./config/multer");
+const auth = require("./middlewares/auth");
 const routes = Router();
+const upload = new Upload();
+const address = new AddressController();
+const client = new ClientController();
+const provider = new ProviderController();
+const scheduling = new SchedulingController();
+const product = new ProductController();
+
+routes.post("/login", new LoginController().login);
 
 // Address routes
-routes.post("/address", new AddressController().create);
-routes.get("/address", new AddressController().getAll);
-routes.get("/address/:id", new AddressController().getOne);
-routes.put("/address/:id", new AddressController().update);
-routes.delete("/address/:id", new AddressController().delete);
+routes.post("/address", auth, address.create);
+routes.get("/address", address.getAll);
+routes.get("/address/:id", address.getOne);
+routes.put("/address/:id", address.update);
+routes.delete("/address/:id", auth, address.delete);
 
 // Client routes
-// cadastro
-routes.post("/client/signup", new ClientController().create)
-// login
-// routes.post("/client/login", new ClientController().login)
-routes.get("/client", new ClientController().getAll);
-routes.get("/client/:id", new ClientController().getOne);
-routes.put("/client/:id", new ClientController().update);
-routes.delete("/client/:id", new ClientController().delete);
+routes.post("/client/signup", client.create);
+routes.get("/client", client.getAll);
+routes.get("/client/:id", client.getOne);
+routes.put(
+  "/client/:id",
+  multer(multerConfig).single("file"),
+  upload.profilePic,
+  client.update
+);
+routes.delete("/client/:id", client.delete);
 
 // Provider routes
-// cadastro
-routes.post("/provider/signup", new ProviderController().create);
-// login
-// routes.post("/provider/login", new ProviderController().login)
-routes.get("/provider", new ProviderController().getAll);
-routes.get("/provider/:id", new ProviderController().getOne);
-routes.put("/provider/:id", new ProviderController().update);
-routes.delete("/provider/:id", new ProviderController().delete);
+routes.post("/provider/signup", provider.create);
+routes.get("/provider", provider.getAll);
+routes.get("/provider/:id", provider.getOne);
+routes.put(
+  "/provider/:id",
+  multer(multerConfig).single("file"),
+  upload.profilePic,
+  provider.update
+);
+routes.delete("/provider/:id", provider.delete);
 
 // Scheduling routes
-routes.post("/scheduling", new SchedulingController().create);
-routes.get("/scheduling", new SchedulingController().getAll);
-routes.get("/scheduling/:id", new SchedulingController().getOne);
-routes.put("/scheduling/:id", new SchedulingController().update);
-routes.delete("/scheduling/:id", new SchedulingController().delete);
+routes.post("/scheduling", scheduling.create);
+routes.get("/scheduling", scheduling.getAll);
+routes.get("/scheduling/:id", scheduling.getOne);
+routes.put("/scheduling/:id", scheduling.update);
+routes.delete("/scheduling/:id", scheduling.delete);
 
 // Products routes
-routes.post("/product", new ProductController().create);
-routes.get("/product", new ProductController().getAll);
-routes.get("/product/:id", new ProductController().getOne);
-routes.put("/product/:id", new ProductController().update);
-routes.delete("/product/:id", new ProductController().delete);
+routes.post("/product", product.create);
+routes.get("/product", product.getAll);
+routes.get("/product/:id", product.getOne);
+routes.put(
+  "/product/:id",
+  multer(multerConfig).single("file"),
+  upload.productPic,
+  product.update
+);
+routes.delete("/product/:id", product.delete);
 
 export { routes };
