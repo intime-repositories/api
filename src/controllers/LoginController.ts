898a7b09dc18fab55975a539d4fd644c02fd4453
schutz-luca <lucaschutzenhofer@hotmail.com>
2022-09-13 02:34:23 -0300

    improve login controller

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -7,45 +7,50 @@ import jwt from "jsonwebtoken";
 
 export class LoginController {
   async login(request: Request, response: Response) {
-    const requester = request.body;
-    const clientService = new ClientService();
-    const providerService = new ProviderService();
+    try {
+      const requester = request.body;
+      const clientService = new ClientService();
+      const providerService = new ProviderService();
 
-    async function getUser(email) {
-      const clientUser = await clientService.getOneByEmail(email);
+      async function getUser(email) {
+        const clientUser = await clientService.getOneByEmail(email);
 
-      if (clientUser) return { ...clientUser, role: "client" };
+        if (clientUser) return { ...clientUser, role: "client" };
 
-      const providerUser = await providerService.getOneByEmail(email);
-      if (providerUser) return { ...providerUser, role: "provider" };
+        const providerUser = await providerService.getOneByEmail(email);
+        if (providerUser) return { ...providerUser, role: "provider" };
 
+      }
+      const user = await getUser(requester.email);
+
+      bcrypt.compare(requester.password, user.password, (err, result) => {
+        if (err)
+          response.status(401).send({ message: "Unauthorized" });
+
+        if (result) {
+          const token = jwt.sign(
+            {
+              id: user.id,
+              email: user.email,
+              fullName: user.fullname,
+              role: user.role
+            },
+            process.env.JWT_SECRET_KEY,
+            {
+              expiresIn: "3d"
+            }
+          );
+          return response
+            .status(200)
+            .send({ message: "Successfully logged", token: token });
+        }
+
+        response.status(401).send({ message: "Unauthorized" });
+      });
     }
-    const user = await getUser(requester.email);
-
-    bcrypt.compare(requester.password, user.password, (err, result) => {
-      if (err) {
-        return response.status(401).send({ message: "Not Authorized" });
-      }
-      if (result) {
-        const token = jwt.sign(
-          {
-            id: user.id,
-            email: user.email,
-            fullName: user.fullname,
-            role: user.role
-          },
-          process.env.JWT_SECRET_KEY,
-          {
-            expiresIn: "3d"
-          }
-        );
-        return response
-          .status(200)
-          .send({ message: "Autenticado com sucesso", token: token });
-      }
-
-      response.status(401).send({ message: "Authetication Error" });
-    });
+    catch (error) {
+      response.status(401).send({ message: "Unauthorized" });
+    }
   }
 
 }
